03d26b4b762a3e8c12dbc71c8aa1f6abd1766a8f
Ilya Beliaev <info@php-dev.info>
2025-10-09 19:35:37 +0200

    fix(mail): ensure handlers implement both detector and handler interfaces in MailReplyScanner

--- config/imap.php
+++ config/imap.php
@@ -48,7 +48,7 @@ return [
 
         'default' => [// account identifier
             'host' => env('MAIL_HOST', 'localhost'),
-            'port' => env('MAIL_PORT', 993),
+            'port' => env('IMAP_PORT', 993),
             'protocol' => env('IMAP_PROTOCOL', 'imap'), //might also use imap, [pop3 or nntp (untested)]
             'encryption' => env('MAIL_ENCRYPTION', 'ssl'), // Supported: false, 'ssl', 'tls', 'notls', 'starttls'
             'validate_cert' => env('IMAP_VALIDATE_CERT', true),
