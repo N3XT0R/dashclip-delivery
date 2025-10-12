f278c9fbaa7cc1ab116af1e59daf5de3accfb18d
Ilya Beliaev <info@php-dev.info>
2025-10-12 21:51:37 +0200

    fix(video-upload): isolate clip selector per repeater instance and fix invalid querySelector
    - Escaped Livewire attribute selector to avoid invalid CSS syntax error
    - Limited file input lookup to local repeater container to ensure per-video isolation
    - Keeps x-data inline and self-contained for clean Alpine integration

--- resources/views/filament/forms/components/clip-selector.blade.php
+++ resources/views/filament/forms/components/clip-selector.blade.php
@@ -1,44 +1,5 @@
 <div
-        x-data="{
-        start: 0,
-        end: 0,
-        duration: 0,
-        showPlayer: false,
-        init() {
-            const input = this.$el.parentElement.querySelector('input[type=file][wire\:model$=file]');
-            input?.addEventListener('change', () => {
-                // hide preview while uploading
-                this.showPlayer = false;
-            });
-            input?.addEventListener('livewire-upload-finish', () => {
-                const file = input.files[0];
-                if (file) {
-                    this.$refs.video.src = URL.createObjectURL(file);
-                    this.showPlayer = true;
-                }
-            });
-            this.$refs.video.addEventListener('loadedmetadata', () => {
-                this.duration = this.$refs.video.duration;
-                if (this.end === 0 || this.end > this.duration) {
-                    this.end = this.duration;
-                }
-                if (typeof noUiSlider !== 'undefined') {
-                    if (this.$refs.slider.noUiSlider) {
-                        this.$refs.slider.noUiSlider.destroy();
-                    }
-                    noUiSlider.create(this.$refs.slider, {
-                        start: [this.start, this.end],
-                        connect: true,
-                        range: { min: 0, max: this.duration },
-                    });
-                    this.$refs.slider.noUiSlider.on('update', (values) => {
-                        this.start = Math.round(values[0]);
-                        this.end = Math.round(values[1]);
-                    });
-                }
-            });
-        }
-    }"
+        x-data="videoClipSelector()"
         class="space-y-2"
 >
     <video x-ref="video" x-show="showPlayer" x-cloak controls class="w-full"></video>
@@ -52,3 +13,52 @@
         Ende: <span x-text="end"></span>s
     </div>
 </div>
+@once
+    @push('scripts')
+        <script>
+            function videoClipSelector() {
+                return {
+                    start: 0,
+                    end: 0,
+                    duration: 0,
+                    showPlayer: false,
+                    init() {
+                        const container = this.$el.closest('[wire\\:key]');
+                        const input = container?.querySelector('input[type=file][wire\\:model$="file"]');
+                        input?.addEventListener('change', () => {
+                            // hide preview while uploading
+                            this.showPlayer = false;
+                        });
+                        input?.addEventListener('livewire-upload-finish', () => {
+                            const file = input.files[0];
+                            if (file) {
+                                this.$refs.video.src = URL.createObjectURL(file);
+                                this.showPlayer = true;
+                            }
+                        });
+                        this.$refs.video.addEventListener('loadedmetadata', () => {
+                            this.duration = this.$refs.video.duration;
+                            if (this.end === 0 || this.end > this.duration) {
+                                this.end = this.duration;
+                            }
+                            if (typeof noUiSlider !== 'undefined') {
+                                if (this.$refs.slider.noUiSlider) {
+                                    this.$refs.slider.noUiSlider.destroy();
+                                }
+                                noUiSlider.create(this.$refs.slider, {
+                                    start: [this.start, this.end],
+                                    connect: true,
+                                    range: {min: 0, max: this.duration},
+                                });
+                                this.$refs.slider.noUiSlider.on('update', (values) => {
+                                    this.start = Math.round(values[0]);
+                                    this.end = Math.round(values[1]);
+                                });
+                            }
+                        })
+                    }
+                };
+            }
+        </script>
+    @endpush
+@endonce
