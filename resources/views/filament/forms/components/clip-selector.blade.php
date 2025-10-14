e83991d234933a06439108272931178f46c514b2
Ilya Beliaev <N3XT0R@users.noreply.github.com>
2025-10-15 00:02:27 +0200

    Capture FilePond video duration for uploads

--- resources/views/filament/forms/components/clip-selector.blade.php
+++ resources/views/filament/forms/components/clip-selector.blade.php
@@ -3,6 +3,7 @@
 
     <input type="hidden" x-model="start" wire:model.defer="{{ $getStatePath() }}.start_sec">
     <input type="hidden" x-model="end" wire:model.defer="{{ $getStatePath() }}.end_sec">
+    <input type="hidden" x-model="duration" wire:model.defer="{{ $getStatePath() }}.duration">
 
     <div x-ref="slider" class="mt-2 w-full h-2 bg-gray-200 rounded"></div>
 
