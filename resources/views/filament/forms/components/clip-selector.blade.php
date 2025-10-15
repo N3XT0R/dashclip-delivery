966f5bc739aa024724fda7c6a716db40b942fe25
Ilya Beliaev <info@php-dev.info>
2025-10-15 08:50:41 +0200

    fix

--- resources/views/filament/forms/components/clip-selector.blade.php
+++ resources/views/filament/forms/components/clip-selector.blade.php
@@ -4,10 +4,4 @@
     <input type="hidden" x-model="start" wire:model.defer="{{ $getStatePath() }}.start_sec">
     <input type="hidden" x-model="end" wire:model.defer="{{ $getStatePath() }}.end_sec">
     <input type="hidden" x-model="duration" wire:model.defer="{{ $getStatePath() }}.duration">
-
-    <div x-ref="slider" class="mt-2 w-full h-2 bg-gray-200 rounded"></div>
-
-    <div class="text-sm">
-        Start: <span x-text="start"></span>s – Ende: <span x-text="end"></span>s
-    </div>
 </div>
