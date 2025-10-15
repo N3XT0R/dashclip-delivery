4e6223759160d5a1ac82b1baea9c572ed5b28b89
Ilya Beliaev <info@php-dev.info>
2025-10-15 08:51:03 +0200

    fix

--- resources/views/filament/forms/components/clip-selector.blade.php
+++ resources/views/filament/forms/components/clip-selector.blade.php
@@ -1,6 +1,4 @@
 <div x-data="clipSelector()" class="space-y-2" x-init="init()">
-    <video x-ref="video" x-show="showPlayer" x-cloak controls class="w-full"></video>
-
     <input type="hidden" x-model="start" wire:model.defer="{{ $getStatePath() }}.start_sec">
     <input type="hidden" x-model="end" wire:model.defer="{{ $getStatePath() }}.end_sec">
     <input type="hidden" x-model="duration" wire:model.defer="{{ $getStatePath() }}.duration">
