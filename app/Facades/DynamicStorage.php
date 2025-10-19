ce2909ad92dda3302d68239e888c7e53e962a21e
Ilya Beliaev <info@php-dev.info>
2025-10-19 14:22:47 +0200

    added: DynamicStorage

--- app/Facades/DynamicStorage.php
+++ app/Facades/DynamicStorage.php
@@ -5,8 +5,12 @@ declare(strict_types=1);
 namespace App\Facades;
 
 use App\Services\DynamicStorageService;
+use Illuminate\Contracts\Filesystem\Filesystem;
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static Filesystem fromPath(string $path)
+ */
 class DynamicStorage extends Facade
 {
     protected static function getFacadeAccessor(): string
