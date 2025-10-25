315193732aa01cfce773cb4f60a11514478bf433
Ilya Beliaev <info@php-dev.info>
2025-10-25 13:02:30 +0200

    refactor(#152): new ingest-service, refactoring videoservice and previewservice.

--- app/Services/Upload/DropboxUploadService.php
+++ app/Services/Upload/DropboxUploadService.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Services\Upload;
 
+use App\Services\Dropbox\AutoRefreshTokenProvider;
 use App\Support\PathBuilder;
 use Illuminate\Contracts\Filesystem\Filesystem;
 use Spatie\Dropbox\Client as DropboxClient;
