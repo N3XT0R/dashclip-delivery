f625fe04004fde125906c3ada1e84fa573f2c961
Ilya Beliaev <info@php-dev.info>
2025-10-25 12:43:23 +0200

    refactor(#152): new ingest-service, refactoring videoservice and previewservice.

--- app/Services/Upload/UploadService.php
+++ app/Services/Upload/UploadService.php
@@ -10,7 +10,6 @@ use Illuminate\Support\Facades\Storage;
 
 class UploadService
 {
-
     public function uploadFile(Filesystem $sourceDisk, string $relativePath, string $targetDisk): void
     {
         if ($targetDisk === 'dropbox') {
