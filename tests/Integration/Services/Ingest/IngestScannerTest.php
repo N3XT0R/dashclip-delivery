6e6b1d0a4665df4026a53200b3846781933e53d6
Ilya Beliaev <info@php-dev.info>
2025-10-29 12:18:31 +0100

    tests(BatchService): changed/added tests.

--- tests/Integration/Services/Ingest/IngestScannerTest.php
+++ tests/Integration/Services/Ingest/IngestScannerTest.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Services\Ingest;
 
+use App\DTO\FileInfoDto;
+use App\Facades\DynamicStorage;
 use App\Services\Ingest\IngestScanner;
 use Tests\DatabaseTestCase;
 
@@ -27,5 +29,24 @@ class IngestScannerTest extends DatabaseTestCase
         $stats = $ingestStats->toArray();
         $this->assertSame(3, $ingestStats->total());
         $this->assertSame(['new' => 3, 'dups' => 0, 'err' => 0], $stats);
+    }
+
+    /**
+     * @throws \Throwable
+     */
+    public function testProcessFileReturnsImportResultWithNew(): void
+    {
+        \Storage::fake('local');
+        $inboxPath = base_path('tests/Fixtures/Inbox/Videos/');
+        $disk = DynamicStorage::fromPath($inboxPath);
+        $fileInfoDto = new FileInfoDto(
+            'standalone.mp4',
+            'standalone.mp4',
+            'mp4'
+        );
+
+        $importResult = $this->ingestScanner->processFile($disk, $fileInfoDto, 'local');
+        $this->assertNotNull($importResult);
+        self::assertSame('NEW', $importResult->name);
     }
 }
