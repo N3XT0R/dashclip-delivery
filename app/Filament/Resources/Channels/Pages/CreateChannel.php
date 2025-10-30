028153e7be686842a530183ef98f172ee1bc0615
Ilya Beliaev <info@php-dev.info>
2025-10-30 14:41:29 +0100

    added channelCreated event.

--- app/Filament/Resources/Channels/Pages/CreateChannel.php
+++ app/Filament/Resources/Channels/Pages/CreateChannel.php
@@ -2,10 +2,21 @@
 
 namespace App\Filament\Resources\Channels\Pages;
 
+use App\Events\ChannelCreated;
 use App\Filament\Resources\Channels\ChannelResource;
+use App\Models\Channel;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateChannel extends CreateRecord
 {
     protected static string $resource = ChannelResource::class;
+
+    protected function afterCreate(): void
+    {
+        /**
+         * @var Channel $record
+         */
+        $record = $this->record;
+        event(new ChannelCreated($record));
+    }
 }
