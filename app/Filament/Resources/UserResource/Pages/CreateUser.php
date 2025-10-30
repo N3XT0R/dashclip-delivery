bc57f1ca591cbac4e735cf1ab089e2963efaa145
Ilya Beliaev <info@php-dev.info>
2025-10-30 23:20:23 +0100

    added: welcome email

--- app/Filament/Resources/UserResource/Pages/CreateUser.php
+++ app/Filament/Resources/UserResource/Pages/CreateUser.php
@@ -5,11 +5,40 @@ namespace App\Filament\Resources\UserResource\Pages;
 use App\Events\User\UserCreated;
 use App\Filament\Resources\UserResource;
 use App\Models\User;
+use Filament\Forms;
 use Filament\Resources\Pages\CreateRecord;
+use Filament\Schemas\Schema;
 
 class CreateUser extends CreateRecord
 {
     protected static string $resource = UserResource::class;
+
+
+    public function form(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                Forms\Components\TextInput::make('name')
+                    ->required(),
+                Forms\Components\TextInput::make('submitted_name'),
+                Forms\Components\TextInput::make('email')
+                    ->label('Email address')
+                    ->email()
+                    ->required(),
+                Forms\Components\DateTimePicker::make('email_verified_at'),
+                Forms\Components\Select::make('roles')
+                    ->label('Roles')
+                    ->multiple()
+                    ->relationship('roles', 'name')
+                    ->preload(),
+                Forms\Components\Textarea::make('app_authentication_secret')
+                    ->columnSpanFull(),
+                Forms\Components\Textarea::make('app_authentication_recovery_codes')
+                    ->columnSpanFull(),
+                Forms\Components\Toggle::make('has_email_authentication')
+                    ->required(),
+            ]);
+    }
 
 
     protected function afterCreate(): void
