f8f80cc83fedd96fd97f5d53e972c490156fe2bb
Ilya Beliaev <info@php-dev.info>
2025-10-30 23:18:19 +0100

    added: welcome email

--- app/Filament/Resources/UserResource/Pages/CreateUser.php
+++ app/Filament/Resources/UserResource/Pages/CreateUser.php
@@ -2,10 +2,22 @@
 
 namespace App\Filament\Resources\UserResource\Pages;
 
+use App\Events\User\UserCreated;
 use App\Filament\Resources\UserResource;
+use App\Models\User;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateUser extends CreateRecord
 {
     protected static string $resource = UserResource::class;
+
+
+    protected function afterCreate(): void
+    {
+        /**
+         * @var User $record
+         */
+        $record = $this->record;
+        event(new UserCreated($record, true));
+    }
 }
