951ca554d78425e8f93368dbeaa6cbd5414d3b32
Ilya Beliaev <info@php-dev.info>
2025-10-30 15:05:42 +0100

    added channelCreated event.

--- database/migrations/2025_10_30_150357_add_approved_at_to_channels_table.php
+++ database/migrations/2025_10_30_150357_add_approved_at_to_channels_table.php
@@ -10,7 +10,7 @@ return new class extends Migration {
      */
     public function up(): void
     {
-        Schema::table('channels', function (Blueprint $table) {
+        Schema::table('channels', static function (Blueprint $table) {
             $table->timestamp('approved_at')->nullable()->after('is_video_reception_paused');
         });
     }
@@ -20,7 +20,7 @@ return new class extends Migration {
      */
     public function down(): void
     {
-        Schema::table('channels', function (Blueprint $table) {
+        Schema::table('channels', static function (Blueprint $table) {
             $table->dropColumn('approved_at');
         });
     }
