a1e47012d6521634b7190b4ca77405cb47063286
Ilya Beliaev <info@php-dev.info>
2025-10-30 01:49:42 +0100

    chore(license): update license from MIT to AGPL-3.0-or-later
    The project license has been changed from MIT to GNU Affero General Public License v3.0 (or later)
    to ensure that all network-accessible or modified versions of dashclip-delivery remain open source.
    
    This change reflects the project's evolution into a full-fledged distributed
    media ingestion and delivery system, protecting its open nature while
    requiring derived works to share their source code.
    
    See CHANGELOG.md entry for details.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -45,6 +45,7 @@
             href="{{route('changelog')}}" target="_blank">{{ config('app.version', '0.0.0-dev') }}</a>
     - <a href="{{ route('impressum') }}" target="_blank">Impressum</a>
     - <a href="{{ route('datenschutz') }}" target="_blank">Datenschutz</a>
+    - <a href="{{ route('license') }}" target="_blank">Lizenz</a>
     - <a href="{{ url(config('app.footer.roadmap')) }}" target="_blank">Roadmap</a>
     - <a href="{{ url(config('app.footer.issues')) }}" target="_blank">Bug gefunden?</a>
     <br/>
