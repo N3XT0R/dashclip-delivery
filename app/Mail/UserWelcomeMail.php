ead7b70bfff69557f39cc8059d2840f959273bda
Ilya Beliaev <info@php-dev.info>
2025-10-30 23:13:05 +0100

    added: welcome email

--- app/Mail/UserWelcomeMail.php
+++ app/Mail/UserWelcomeMail.php
@@ -4,8 +4,23 @@ declare(strict_types=1);
 
 namespace App\Mail;
 
+use App\Models\User;
+use Illuminate\Mail\Mailables\Envelope;
+
 class UserWelcomeMail extends AbstractLoggedMail
 {
+    public function __construct(
+        public User $user
+    ) {
+    }
+
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Willkommen bei '.config('app.name'),
+        );
+    }
+
     protected function viewName(): string
     {
         // TODO: Implement viewName() method.
