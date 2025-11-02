e865121e167b6a4a9ef155de536ca1883dbb663a
Ilya Beliaev <info@php-dev.info>
2025-11-02 09:56:48 +0100

    emailOutput removed.

--- routes/console.php
+++ routes/console.php
@@ -13,18 +13,15 @@ Artisan::command('inspire', function () {
 
 Schedule::command('weekly:run')
     ->mondays()
-    ->at('08:00')
-    ->emailOutputTo($email);
+    ->at('08:00');
 
 Schedule::command('ingest:scan', [
     '--inbox' => Cfg::get('ingest_inbox_absolute_path', 'default', '/srv/ingest/pending/', true),
-])->hourly()
-    ->emailOutputOnFailure($email);
+])->hourly();
 
 Schedule::command('ingest:unzip', [
     '--inbox' => Cfg::get('ingest_inbox_absolute_path', 'default', '/srv/ingest/pending/', true),
-])->everyTenMinutes()
-    ->emailOutputOnFailure($email);
+])->everyTenMinutes();
 
 Schedule::command('assign:expire')
     ->dailyAt('03:00');
@@ -32,14 +29,12 @@ Schedule::command('assign:expire')
 Schedule::command('video:cleanup', [
     '--weeks' => Cfg::get('post_expiry_retention_weeks', 'default', 1, true),
 ])
-    ->dailyAt('04:00')
-    ->emailOutputOnFailure($email);
+    ->dailyAt('04:00');
 
 Schedule::command('notify:reminders', [
     '--days' => Cfg::get('email_reminder_days', 'email', 1, true),
 ])
-    ->dailyAt('09:00')
-    ->emailOutputOnFailure($email);
+    ->dailyAt('09:00');
 
 Schedule::command('dropbox:refresh-token')
     ->everyMinute();
