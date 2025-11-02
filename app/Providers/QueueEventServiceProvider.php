b4d30cf085c0c3c60e29bf6fae10e31e5a0f2fde
Ilya Beliaev <info@php-dev.info>
2025-11-02 10:30:41 +0100

    added: logging

---
+++ app/Providers/QueueEventServiceProvider.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Queue\Events\JobFailed;
+use Illuminate\Queue\Events\JobProcessed;
+use Illuminate\Support\Facades\Queue;
+use Illuminate\Support\ServiceProvider;
+
+class QueueEventServiceProvider extends ServiceProvider
+{
+    public function boot(): void
+    {
+        // Log successful jobs
+        Queue::after(static function (JobProcessed $event) {
+            $job = $event->job;
+            if ($job->getQueue() !== 'default') {
+                return;
+            }
+            $payload = $job->payload();
+
+            activity()
+                ->withProperties([
+                    'job_id' => $job->getJobId(),
+                    'name' => $job->getName(),
+                    'job' => $payload['displayName'] ?? get_class($job),
+                    'queue' => $job->getQueue(),
+                    'status' => 'completed',
+                ])
+                ->log('Job completed');
+        });
+
+        // Log failed jobs
+        Queue::failing(static function (JobFailed $event) {
+            $job = $event->job;
+            if ($job->getQueue() !== 'default') {
+                return;
+            }
+            $payload = $job->payload();
+
+            activity()
+                ->withProperties([
+                    'job_id' => $job->getJobId(),
+                    'name' => $job->getName(),
+                    'job' => $payload['displayName'] ?? get_class($job),
+                    'queue' => $job->getQueue(),
+                    'status' => 'failed',
+                    'exception' => $event->exception?->getMessage(),
+                ])
+                ->log('Job failed');
+        });
+    }
+}
