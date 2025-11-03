98d396cdb0181942e9e715abd140fde407752728
Ilya Beliaev <info@php-dev.info>
2025-11-03 18:45:32 +0100

    feat(config): added new email_reminder_flag.

---
+++ database/migrations/2025_11_03_184407_add_email_reminder_config.php
@@ -0,0 +1,32 @@
+<?php
+
+use Carbon\Carbon;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        $timestamp = Carbon::now()->format('Y-m-d H:i:s');
+        $categoryId = DB::table('config_categories')->where('slug', 'email')->value('id');
+        DB::table('configs')->insert([
+            'key' => 'email_reminder',
+            'value' => 2,
+            'cast_type' => 'bool',
+            'config_category_id' => $categoryId,
+            'created_at' => $timestamp,
+            'updated_at' => $timestamp,
+            'is_visible' => 1,
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        DB::table('configs')->where('key', 'email_reminder')->delete();
+    }
+};
