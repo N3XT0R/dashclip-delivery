3d5fed913ae2265cc640074216911a5379a6e3ce
Ilya Beliaev <info@php-dev.info>
2025-11-17 00:29:27 +0100

    refactor: move SQL/Eloquent queries from AssignmentDistributor.php into AssignmentRepository.php for cleaner separation of concerns (#121)

--- tests/Integration/Services/AssignmentDistributorBundleTest.php
+++ tests/Integration/Services/AssignmentDistributorBundleTest.php
@@ -9,14 +9,22 @@ use Tests\DatabaseTestCase;
 class AssignmentDistributorBundleTest extends DatabaseTestCase
 {
 
-    public function test_bundle_videos_are_assigned_together(): void
+    protected AssignmentDistributor $assignmentDistributor;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->assignmentDistributor = $this->app->make(AssignmentDistributor::class);
+    }
+
+    public function testBundleVideosAreAssignedTogether(): void
     {
         $v1 = Video::create(['hash' => 'h1', 'path' => 'p1']);
         $v2 = Video::create(['hash' => 'h2', 'path' => 'p2']);
         Clip::create(['video_id' => $v1->id, 'bundle_key' => 'B']);
         Clip::create(['video_id' => $v2->id, 'bundle_key' => 'B']);
 
-        $result = (new AssignmentDistributor())->distribute();
+        $result = $this->assignmentDistributor->distribute();
 
         $this->assertSame(2, $result['assigned']);
         $assignments = Assignment::query()->whereIn('video_id', [$v1->id, $v2->id])->get();
