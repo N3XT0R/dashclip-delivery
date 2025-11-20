ce16bcfde557ca1c3cb7a91ce246c7469c9beabe
Ilya Beliaev <info@php-dev.info>
2025-11-20 22:13:50 +0100

    added new panel.

--- app/Providers/Filament/StandardPanelProvider.php
+++ app/Providers/Filament/StandardPanelProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers\Filament;
 
+use App\Filament\Pages\Auth\EditProfile;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
@@ -38,6 +39,7 @@ class StandardPanelProvider extends PanelProvider
             ->login()
             ->emailVerification()
             ->emailChangeVerification()
+            ->profile(EditProfile::class, false)
             ->colors([
                 'primary' => Color::Sky,
             ])
