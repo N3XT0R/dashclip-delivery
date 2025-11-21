d07abde73623daa8c9954d0e045785bb8c2888fd
Ilya Beliaev <info@php-dev.info>
2025-11-21 01:20:49 +0100

    added new panel.

--- app/Filament/Standard/Pages/Auth/Register.php
+++ app/Filament/Standard/Pages/Auth/Register.php
@@ -4,9 +4,20 @@ declare(strict_types=1);
 
 namespace App\Filament\Standard\Pages\Auth;
 
+use App\Enum\Users\RoleEnum;
+use App\Models\User;
 use Filament\Auth\Pages\Register as BaseRegister;
 
 class Register extends BaseRegister
 {
+    protected function handleRegistration(array $data): User
+    {
+        /**
+         * @var User $user
+         */
+        $user = parent::handleRegistration($data);
+        $user->syncRoles([RoleEnum::REGULAR->value]);
 
+        return $user;
+    }
 }
