e98f7aa6e57ba5233bdf0a1770ccc6452fb8736e
Ilya Beliaev <info@php-dev.info>
2025-11-22 04:29:21 +0100

    added new panel.

--- app/Models/User.php
+++ app/Models/User.php
@@ -16,7 +16,6 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\MorphToMany;
 use Illuminate\Database\Query\Builder;
@@ -161,11 +160,6 @@ class User extends Authenticatable implements FilamentUser, HasAppAuthentication
             Team::class,
             'team_user'
         )->withTimestamps();
-    }
-
-    public function latestTeam(): BelongsTo
-    {
-        return $this->belongsTo(Team::class);
     }
 
     public function getTenants(Panel $panel): Collection
