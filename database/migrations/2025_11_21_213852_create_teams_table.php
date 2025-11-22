34eaf6bfeb2749779cca9061463c77eb10fac8c6
Ilya Beliaev <info@php-dev.info>
2025-11-22 01:45:53 +0100

    added new panel.

--- database/migrations/2025_11_21_213852_create_teams_table.php
+++ database/migrations/2025_11_21_213852_create_teams_table.php
@@ -12,6 +12,7 @@ return new class extends Migration {
     {
         Schema::create('teams', static function (Blueprint $table) {
             $table->id();
+            $table->string('slug')->unique();
             $table->string('name');
             $table->foreignId('owner_id')->constrained('users')->cascadeOnDelete();
             $table->timestamps();
