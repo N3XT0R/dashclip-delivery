0bd39ccac4708532606036bd4468149af9909971
Ilya Beliaev <info@php-dev.info>
2025-11-22 01:39:59 +0100

    added new panel.

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -55,6 +55,7 @@ class AdminPanelProvider extends PanelProvider
             ->brandLogoHeight('75px')
             ->id('admin')
             ->path('admin')
+            ->authGuard('web')
             ->tenant(null)
             ->login()
             ->emailVerification()
