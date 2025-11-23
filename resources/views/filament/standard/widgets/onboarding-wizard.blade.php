94292e778550a220461abb5326672ab014f064a7
Ilya Beliaev <info@php-dev.info>
2025-11-23 22:41:43 +0100

    added relation for channel/team.

--- resources/views/filament/standard/widgets/onboarding-wizard.blade.php
+++ resources/views/filament/standard/widgets/onboarding-wizard.blade.php
@@ -1,19 +1,24 @@
-<x-filament::modal id="onboarding-wizard" width="3xl" alignment="center">
-    <x-slot name="heading">
-        Willkommen! Lass uns dein Profil einrichten.
-    </x-slot>
+<x-filament-widgets::widget>
+    <div class="text-red-500 font-bold">
+        MODAL VIEW LOADED
+    </div>
+    <x-filament::modal id="onboarding-wizard" width="3xl" alignment="center">
+        <x-slot name="heading">
+            Willkommen! Lass uns dein Profil einrichten.
+        </x-slot>
 
-    <x-slot name="description">
-        Bitte führe die folgenden Schritte durch, um dein Konto vollständig zu aktivieren.
-    </x-slot>
+        <x-slot name="description">
+            Bitte führe die folgenden Schritte durch, um dein Konto vollständig zu aktivieren.
+        </x-slot>
 
-    <form wire:submit.prevent="submit">
-        {{ $this->schema ?? $this->form }}
-    </form>
+        <form wire:submit.prevent="submit">
+            {{ $this->schema ?? $this->form }}
+        </form>
 
-    <x-slot name="footer">
-        <x-filament::button type="submit" color="primary">
-            Abschließen
-        </x-filament::button>
-    </x-slot>
-</x-filament::modal>
+        <x-slot name="footer">
+            <x-filament::button type="submit" color="primary">
+                Abschließen
+            </x-filament::button>
+        </x-slot>
+    </x-filament::modal>
+</x-filament-widgets::widget>
