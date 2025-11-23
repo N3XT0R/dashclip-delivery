74073412372c029fc0b52ea1ec929933db98c56f
Ilya Beliaev <info@php-dev.info>
2025-11-23 02:38:36 +0100

    added new panel.

--- app/Filament/Standard/Resources/VideoResource/Pages/ViewVideo.php
+++ app/Filament/Standard/Resources/VideoResource/Pages/ViewVideo.php
@@ -4,6 +4,7 @@ namespace App\Filament\Standard\Resources\VideoResource\Pages;
 
 use App\Filament\Standard\Resources\VideoResource;
 use Filament\Resources\Pages\ViewRecord;
+use Illuminate\Database\Eloquent\Model;
 
 class ViewVideo extends ViewRecord
 {
@@ -12,5 +13,15 @@ class ViewVideo extends ViewRecord
     protected function getHeaderActions(): array
     {
         return [];
+    }
+
+    protected function resolveRecord(int|string $key): Model
+    {
+        return static::getModel()::query()
+            ->select('videos.*')
+            ->join('clips', 'videos.id', '=', 'clips.video_id')
+            ->where('clips.user_id', auth()->id())
+            ->where('videos.id', $key)
+            ->firstOrFail();
     }
 }
