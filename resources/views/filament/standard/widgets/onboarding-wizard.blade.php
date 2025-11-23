f08cc1d9e8451d232e51aaccc55963415e081a0c
Ilya Beliaev <info@php-dev.info>
2025-11-23 22:20:11 +0100

    added relation for channel/team.

--- resources/views/filament/standard/widgets/onboarding-wizard.blade.php
+++ resources/views/filament/standard/widgets/onboarding-wizard.blade.php
@@ -1,4 +1,4 @@
-<x-filament::modal id="onboarding-wizard" width="3xl">
+<x-filament::modal id="onboarding-wizard" width="3xl" alignment="center">
     <x-slot name="heading">
         Willkommen! Lass uns dein Profil einrichten.
     </x-slot>
