27d6ffcb325adc9191bb27e631e86bc409c1e153
Ilya Beliaev <info@php-dev.info>
2025-11-25 23:50:09 +0100

    added tests.

--- tests/Integration/Console/ScanMailRepliesCommandTest.php
+++ tests/Integration/Console/ScanMailRepliesCommandTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Tests\Integration\Console;
 
+use App\Services\Mail\Scanner\MailReplyScanner;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Cache;
 use Mockery;
@@ -20,6 +21,11 @@ class ScanMailRepliesCommandTest extends DatabaseTestCase
 
     public function testItInvokesScannerWhenFaqEmailIsConfigured(): void
     {
+        $scanner = Mockery::mock(MailReplyScanner::class);
+        $scanner->shouldReceive('scan')->once();
+
+        $this->app->instance(MailReplyScanner::class, $scanner);
+
         $this->artisan('mail:scan-replies')
             ->assertExitCode(Command::SUCCESS);
     }
