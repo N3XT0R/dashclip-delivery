2ca58f75ab9d92a05c143577c6af94657b9bb2c7
Ilya Beliaev <info@php-dev.info>
2025-11-29 03:03:36 +0100

    added tests.

--- tests/Feature/Filament/Standard/Pages/SelectChannelsTest.php
+++ tests/Feature/Filament/Standard/Pages/SelectChannelsTest.php
@@ -7,6 +7,8 @@ namespace Tests\Feature\Filament\Standard\Pages;
 use App\Enum\Guard\GuardEnum;
 use App\Filament\Standard\Pages\SelectChannels;
 use App\Models\User;
+use App\Repository\TeamRepository;
+use Filament\Facades\Filament;
 use Livewire\Livewire;
 use Tests\DatabaseTestCase;
 
@@ -14,12 +16,14 @@ class SelectChannelsTest extends DatabaseTestCase
 {
     public function testRegularUserHasAccess(): void
     {
+        User::flushEventListeners();
         $guard = GuardEnum::STANDARD;
         $regularUser = User::factory()
             ->withOwnTeam()
             ->standard($guard)
             ->create();
-
+        $tenant = $this->app->make(TeamRepository::class)->getDefaultTeamForUser($regularUser);
+        Filament::setTenant($tenant, true);
         $this->actingAs($regularUser, $guard->value);
 
         Livewire::test(SelectChannels::class)
