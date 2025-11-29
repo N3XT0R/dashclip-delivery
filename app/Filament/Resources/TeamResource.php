8bc1efe9c16aca94b9cfee63899051ac856bd2c3
Ilya Beliaev <info@php-dev.info>
2025-11-29 16:55:29 +0100

    test added.

--- app/Filament/Resources/TeamResource.php
+++ app/Filament/Resources/TeamResource.php
@@ -18,7 +18,7 @@ class TeamResource extends Resource
 {
     protected static ?string $model = Team::class;
 
-    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedUserGroup;
 
     protected static string|UnitEnum|null $navigationGroup = 'System';
 
