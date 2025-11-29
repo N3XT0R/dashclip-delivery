6f2e22b2a25dc4d030de415e16bf769281908e61
Ilya Beliaev <info@php-dev.info>
2025-11-29 02:22:35 +0100

    added tests.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,6 @@ class DatabaseSeeder extends Seeder
         $this->call([
             TeamSeeder::class,
             ShieldSeeder::class,
-            AdminSeeder::class,
-            RegularSeeder::class,
         ]);
     }
 }
