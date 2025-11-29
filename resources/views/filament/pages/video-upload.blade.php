770f80dcd79d99e3af6c26af3514adfc07d6847c
Ilya Beliaev <info@php-dev.info>
2025-11-30 00:52:08 +0100

    fix.

--- resources/views/filament/pages/video-upload.blade.php
+++ resources/views/filament/pages/video-upload.blade.php
@@ -1,9 +1,29 @@
 <x-filament-panels::page>
     <form wire:submit.prevent="submit" class="space-y-6">
         {{ $this->form }}
-        <x-filament::button type="submit">
+        <x-filament::button type="submit" id="submit">
             Upload
         </x-filament::button>
     </form>
 </x-filament-panels::page>
 
+<script>
+    const submit = document.getElementById('submit');
+    document.addEventListener('FilePond:processfilestart', () => toggleButton(submit, true));
+    document.addEventListener('FilePond:processfileprogress', function (e) {
+        let detail = e.detail;
+        if (detail.progress === 1) {
+            toggleButton(submit, false);
+        } else {
+            toggleButton(submit, true);
+        }
+    });
+    document.addEventListener('FilePond:processfile', () => toggleButton(submit, false));
+    document.addEventListener('FilePond:processfileabort', () => toggleButton(submit, false));
+    document.addEventListener('FilePond:error', () => toggleButton(submit, false));
+
+
+    function toggleButton(button, disabled) {
+        button.disabled = disabled;
+    }
+</script>
