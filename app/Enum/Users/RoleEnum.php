86c608462f60cc1b5616abd1f37dd1de843927e2
Ilya Beliaev <info@php-dev.info>
2025-12-03 23:49:07 +0100

    removed not used code.

--- app/Enum/Users/RoleEnum.php
+++ app/Enum/Users/RoleEnum.php
@@ -9,12 +9,5 @@ enum RoleEnum: string
     case SUPER_ADMIN = 'super_admin';
 
     case REGULAR = 'panel_user';
-
-    public static function getRoles(): array
-    {
-        return [
-            self::SUPER_ADMIN,
-            self::REGULAR,
-        ];
-    }
+    
 }
