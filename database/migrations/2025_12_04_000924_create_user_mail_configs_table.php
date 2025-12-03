d03fdab70f8303990f39ba8d078710dfeeccff79
Ilya Beliaev <info@php-dev.info>
2025-12-04 00:11:17 +0100

    added new migration.

--- database/migrations/2025_12_04_000924_create_user_mail_configs_table.php
+++ database/migrations/2025_12_04_000924_create_user_mail_configs_table.php
@@ -4,22 +4,22 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('user_mail_configs', static function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+
+            $table->string('key');
+            $table->boolean('value')->default(true);
+
             $table->timestamps();
+            
+            $table->unique(['user_id', 'key']);
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('user_mail_configs');
