22ee66d11ae4b178159ba9ca994159b3620f3bb3
Ilya Beliaev <info@php-dev.info>
2025-12-05 21:30:07 +0100

    fix: footer position

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,5 @@
 import './bootstrap';
 import ZipDownloader from './components/ZipDownloader';
-import {run} from 'cookieconsent';
 import 'cookieconsent/build/cookieconsent.min.css';
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -8,33 +7,4 @@ document.addEventListener('DOMContentLoaded', () => {
     if (form) {
         new ZipDownloader(form);
     }
-});
-
-document.addEventListener("DOMContentLoaded", () => {
-    run({
-        categories: {
-            necessary: {
-                enabled: true,
-                readOnly: true
-            }
-        },
-        language: {
-            default: 'de',
-            translations: {
-                de: {
-                    consentModal: {
-                        title: "Cookies",
-                        description: "Wir verwenden ausschließlich technisch notwendige Cookies.",
-                        acceptAllBtn: "OK",
-                        closeIconLabel: "Schließen"
-                    },
-                    preferencesModal: {
-                        title: "Cookie-Einstellungen",
-                        acceptNecessaryBtn: "Nur notwendige Cookies",
-                        acceptAllBtn: "Alle akzeptieren"
-                    }
-                }
-            }
-        }
-    });
 });
