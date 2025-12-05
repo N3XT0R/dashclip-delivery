eebcacf8b51851d26fdb110667faf3fcb3f4859a
Ilya Beliaev <info@php-dev.info>
2025-12-05 21:37:03 +0100

    fix: footer position

--- resources/views/components/cookie-banner.blade.php
+++ resources/views/components/cookie-banner.blade.php
@@ -14,7 +14,7 @@
 
             <button
                     id="cookie-accept"
-                    class="bg-primary-600 hover:bg-primary-700 text-white text-sm px-4 py-2 rounded"
+                    class="bg-slate-600 hover:bg-slate-700 text-white text-sm px-4 py-2 rounded"
             >
                 OK
             </button>
