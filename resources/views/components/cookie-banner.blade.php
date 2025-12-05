0c5c80adf48eb52d7b517ea98c1cb63dd33f1fde
Ilya Beliaev <info@php-dev.info>
2025-12-05 21:35:00 +0100

    fix: footer position

--- resources/views/components/cookie-banner.blade.php
+++ resources/views/components/cookie-banner.blade.php
@@ -4,8 +4,12 @@
             class="fixed bottom-0 inset-x-0 z-[9999] bg-gray-900 text-white p-4 shadow-lg"
     >
         <div class="max-w-4xl mx-auto flex flex-col md:flex-row items-center justify-between gap-3">
-            <span class="text-sm">
+            <span class="text-sm leading-snug">
                 Diese Website verwendet ausschließlich technisch notwendige Cookies.
+                Weitere Informationen findest du in unserer
+                <a href="{{route('datenschutz')}}" class="underline text-gray-300 hover:text-white" target="_blank">
+                    Datenschutzerklärung
+                </a>.
             </span>
 
             <button
