6b787abc9fc0304bda070a1e2448bd42f58f2def
Ilya Beliaev <info@php-dev.info>
2025-12-06 13:21:58 +0100

    i18n + navigationgroups.

--- app/Repository/ChannelRepository.php
+++ app/Repository/ChannelRepository.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Repository;
 
 use App\Models\Channel;
+use App\Models\ChannelApplication;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Support\Collection as SupportCollection;
@@ -79,5 +80,11 @@ class ChannelRepository
     public function getUserAssignedChannels(User $user): SupportCollection
     {
         return $user->assignedChannels()->get();
+    }
+
+    // ...
+    public function createApplication(array $attributes): ChannelApplication
+    {
+        return ChannelApplication::create($attributes);
     }
 }
