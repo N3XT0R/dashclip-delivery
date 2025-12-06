de5278c23634b3abcf70331f98ad10b9afbf0a26
Ilya Beliaev <info@php-dev.info>
2025-12-06 13:35:55 +0100

    i18n + navigationgroups.

--- resources/views/filament/standard/pages/channel-application.blade.php
+++ resources/views/filament/standard/pages/channel-application.blade.php
@@ -3,30 +3,22 @@
         <h2 class="text-lg font-semibold mb-2">
             {{ __('filament.channel_application.form.about_title') }}
         </h2>
-        <p class="mb-2">
+        <p class="mb-4">
             {{ __('filament.channel_application.form.about_intro') }}
         </p>
-        <div class="space-y-3 mb-2">
-            <div class="flex items-start gap-2">
-                <x-filament::icon name="heroicon-o-check-circle" class="w-5 h-5 text-primary-600 shrink-0"/>
-                <span>{{ __('filament.channel_application.form.about_benefit_security') }}</span>
-            </div>
-            <div class="flex items-start gap-2">
-                <x-filament::icon name="heroicon-o-check-circle" class="w-5 h-5 text-primary-600 shrink-0"/>
-                <span>{{ __('filament.channel_application.form.about_benefit_control') }}</span>
-            </div>
-            <div class="flex items-start gap-2">
-                <x-filament::icon name="heroicon-o-check-circle" class="w-5 h-5 text-primary-600 shrink-0"/>
-                <span>{{ __('filament.channel_application.form.about_benefit_remain') }}</span>
-            </div>
+        <div class="mb-4 space-y-2">
+            <p><strong>{{ __('filament.channel_application.form.about_benefit_security_title') }}</strong><br>
+                {{ __('filament.channel_application.form.about_benefit_security') }}</p>
+            <p><strong>{{ __('filament.channel_application.form.about_benefit_control_title') }}</strong><br>
+                {{ __('filament.channel_application.form.about_benefit_control') }}</p>
+            <p><strong>{{ __('filament.channel_application.form.about_benefit_remain_title') }}</strong><br>
+                {{ __('filament.channel_application.form.about_benefit_remain') }}</p>
         </div>
         <p class="text-sm text-gray-500">
             {{ __('filament.channel_application.form.about_footer') }}
         </p>
     </div>
-
     {{ $this->form }}
-
     <x-filament::button type="submit" color="primary" form="form">
         {{ __('filament.channel_application.form.submit') }}
     </x-filament::button>
