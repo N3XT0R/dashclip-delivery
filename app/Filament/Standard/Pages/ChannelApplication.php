3a63891002b1e826f7669b36c1780c2bb6faf7d4
Ilya Beliaev <info@php-dev.info>
2025-12-06 13:44:41 +0100

    i18n + navigationgroups.

--- app/Filament/Standard/Pages/ChannelApplication.php
+++ app/Filament/Standard/Pages/ChannelApplication.php
@@ -5,8 +5,10 @@ namespace App\Filament\Standard\Pages;
 use App\Repository\ChannelRepository;
 use App\Services\ChannelService;
 use BackedEnum;
+use Filament\Forms\Components\Checkbox;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TextInput;
 use Filament\Forms\Concerns\InteractsWithForms;
 use Filament\Forms\Contracts\HasForms;
 use Filament\Notifications\Notification;
@@ -49,12 +51,42 @@ class ChannelApplication extends Page implements HasForms
     {
         return $schema
             ->schema([
+                Checkbox::make('other_channel_request')
+                    ->label(__('filament.channel_application.form.request_other_channel'))
+                    ->reactive(),
                 Select::make('channel_id')
                     ->label('Channel')
                     ->options(function () {
                         return app(ChannelRepository::class)->getActiveChannels()->pluck('name', 'id')->toArray();
                     })
-                    ->required(),
+                    ->searchable()
+                    ->hidden(fn($get) => $get('other_channel_request'))
+                    ->required(fn($get) => !$get('other_channel_request')),
+                TextInput::make('new_channel_name')
+                    ->label(__('filament.channel_application.form.new_channel_name_label'))
+                    ->placeholder(__('filament.channel_application.form.new_channel_name_placeholder'))
+                    ->visible(fn($get) => $get('other_channel_request'))
+                    ->required(fn($get) => $get('other_channel_request')),
+
+                TextInput::make('new_channel_slug')
+                    ->label(__('filament.channel_application.form.new_channel_slug_label'))
+                    ->placeholder(__('filament.channel_application.form.new_channel_slug_placeholder'))
+                    ->visible(fn($get) => $get('other_channel_request'))
+                    ->required(fn($get) => $get('other_channel_request')),
+
+                TextInput::make('new_channel_email')
+                    ->label(__('filament.channel_application.form.new_channel_email_label'))
+                    ->type('email')
+                    ->placeholder(__('filament.channel_application.form.new_channel_email_placeholder'))
+                    ->visible(fn($get) => $get('other_channel_request'))
+                    ->required(fn($get) => $get('other_channel_request')),
+
+                Textarea::make('new_channel_description')
+                    ->label(__('filament.channel_application.form.new_channel_description_label'))
+                    ->placeholder(__('filament.channel_application.form.new_channel_description_placeholder'))
+                    ->rows(4)
+                    ->visible(fn($get) => $get('other_channel_request'))
+                    ->required(fn($get) => $get('other_channel_request')),
                 Textarea::make('note')
                     ->label('filament.channel_application.form.note_label')
                     ->translateLabel()
