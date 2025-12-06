2015cc0807939f73d6f8bd8731b706332753aebc
Ilya Beliaev <info@php-dev.info>
2025-12-06 13:07:25 +0100

    i18n + navigationgroups.

---
+++ app/Models/ChannelApplication.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class ChannelApplication extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'channel_id',
+        'status',
+        'meta',
+    ];
+
+    // Relationships
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function channel(): BelongsTo
+    {
+        return $this->belongsTo(Channel::class);
+    }
+}
