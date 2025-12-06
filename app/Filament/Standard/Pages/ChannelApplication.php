e8aad5ac55dc5998bde7ad9bb8d535cf98580ca5
Ilya Beliaev <info@php-dev.info>
2025-12-06 13:11:42 +0100

    i18n + navigationgroups.

--- app/Filament/Standard/Pages/ChannelApplication.php
+++ app/Filament/Standard/Pages/ChannelApplication.php
@@ -3,13 +3,19 @@
 namespace App\Filament\Standard\Pages;
 
 use BackedEnum;
+use Filament\Forms\Concerns\InteractsWithForms;
+use Filament\Forms\Contracts\HasForms;
 use Filament\Pages\Page;
+use Filament\Schemas\Schema;
 use Filament\Support\Icons\Heroicon;
 use Illuminate\Contracts\Support\Htmlable;
 use UnitEnum;
 
-class ChannelApplication extends Page
+class ChannelApplication extends Page implements HasForms
 {
+
+    use InteractsWithForms;
+
     protected string $view = 'filament.standard.pages.channel-application';
     protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedPencil;
 
@@ -17,6 +23,7 @@ class ChannelApplication extends Page
     protected static ?string $navigationLabel = 'filament.channel_application.navigation_label';
     protected static string|UnitEnum|null $navigationGroup = 'filament.channel_application.navigation_group';
 
+    public ?array $data = [];
 
     public function getTitle(): string|Htmlable
     {
@@ -31,5 +38,14 @@ class ChannelApplication extends Page
     public static function getNavigationGroup(): string
     {
         return __(static::$navigationGroup);
+    }
+
+    public function form(Schema $schema): Schema
+    {
+        return $schema
+            ->schema([
+                
+            ])
+            ->statePath('data');
     }
 }
