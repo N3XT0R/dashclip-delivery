da2bd18006969586400ea67a629800c02ded5054
Ilya Beliaev <info@php-dev.info>
2025-12-06 12:52:51 +0100

    i18n + navigationgroups.

---
+++ app/Filament/Standard/Pages/ChannelApplication.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Filament\Standard\Pages;
+
+use Filament\Pages\Page;
+
+class ChannelApplication extends Page
+{
+    protected string $view = 'filament.standard.pages.channel-application';
+}
