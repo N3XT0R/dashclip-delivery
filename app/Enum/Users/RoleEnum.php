50a26ced82999b7471a1613cb003c68eba8daeb4
Ilya Beliaev <info@php-dev.info>
2025-12-07 14:13:03 +0100

    feat(#131): implementing feature.

--- app/Enum/Users/RoleEnum.php
+++ app/Enum/Users/RoleEnum.php
@@ -7,7 +7,7 @@ namespace App\Enum\Users;
 enum RoleEnum: string
 {
     case SUPER_ADMIN = 'super_admin';
-
     case REGULAR = 'panel_user';
-    
+    case CHANNEL_OPERATOR = 'channel_operator';
+
 }
