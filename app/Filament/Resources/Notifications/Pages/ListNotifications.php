d369f5bf7750d81b6aa4fb1b91151476e7e0f587
Ilya Beliaev <info@php-dev.info>
2025-12-13 10:59:42 +0100

    feat(#131): implementing feature.

--- app/Filament/Resources/Notifications/Pages/ListNotifications.php
+++ app/Filament/Resources/Notifications/Pages/ListNotifications.php
@@ -5,6 +5,11 @@ namespace App\Filament\Resources\Notifications\Pages;
 use App\Filament\Resources\Notifications\NotificationResource;
 use Filament\Resources\Pages\ListRecords;
 
+/**
+ * @extends ListRecords<NotificationResource>
+ * @deprecated will be removed in next major release
+ * @note replaced by mail notifications
+ */
 class ListNotifications extends ListRecords
 {
     protected static string $resource = NotificationResource::class;
