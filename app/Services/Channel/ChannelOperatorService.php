0d07e112eb290fe81541bd8f304b391254cd177b
Ilya Beliaev <info@php-dev.info>
2025-12-13 10:52:10 +0100

    feat(#131): implementing feature.

---
+++ app/Services/Channel/ChannelOperatorService.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services\Channel;
+
+use App\Repository\ChannelRepository;
+use App\Repository\UserRepository;
+
+class ChannelOperatorService
+{
+
+    public function __construct(private UserRepository $userRepository, private ChannelRepository $channelRepository)
+    {
+    }
+}
