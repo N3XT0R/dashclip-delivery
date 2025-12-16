0d96cbf5e33c37875b41dc025052702f5fdfb8d1
Ilya Beliaev <info@php-dev.info>
2025-12-16 21:12:54 +0100

    fix.

--- config/filesystems.php
+++ config/filesystems.php
@@ -61,6 +61,7 @@ return [
         'uploads' => [
             'driver' => 'local',
             'root' => storage_path('app/uploads'),
+            'serve' => true,
             'throw' => false,
             'report' => false,
             'permissions' => [
