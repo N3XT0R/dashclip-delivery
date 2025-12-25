db442a9b04911e521559ea241d8a4129a184790a
Ilya Beliaev <info@php-dev.info>
2025-12-25 22:00:33 +0100

    feat(#131): implementing feature.

--- app/Auth/Abilities/AccessChannelPageAbility.php
+++ app/Auth/Abilities/AccessChannelPageAbility.php
@@ -4,13 +4,12 @@ declare(strict_types=1);
 
 namespace App\Auth\Abilities;
 
-use App\Auth\Abilities\Contracts\AbilityContract;
 use App\Enum\Users\RoleEnum;
 use App\Models\User;
 use App\Repository\ChannelRepository;
 use App\Repository\RoleRepository;
 
-final readonly class AccessChannelPageAbility implements AbilityContract
+final readonly class AccessChannelPageAbility
 {
     public function __construct(
         private RoleRepository $roles,
