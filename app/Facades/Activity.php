2c340154baac15e1140230a841164f1b05fd1087
Ilya Beliaev <info@php-dev.info>
2025-12-25 15:31:20 +0100

    feat(#131): implementing feature.

--- app/Facades/Activity.php
+++ app/Facades/Activity.php
@@ -7,6 +7,9 @@ namespace App\Facades;
 use App\Services\ActivityService;
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static void createActivityLog(string $logName, \App\Models\User $user, string $activityType, \Illuminate\Support\Collection|array $details = [])
+ */
 class Activity extends Facade
 {
     protected static function getFacadeAccessor(): string
--- app/Services/Channel/ChannelApplicationService.php
+++ app/Services/Channel/ChannelApplicationService.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Services\Channel;
 
+use App\Facades\Activity;
 use App\Models\ChannelApplication as ChannelApplicationModel;
 use App\Models\User;
 use App\Services\ChannelService;
@@ -23,6 +24,19 @@ class ChannelApplicationService
             $channel = $channelService->createNewChannelByChannelApplication($channelApplication);
         } else {
             $channel = $channelApplication->channel;
+        }
+
+        if ($user) {
+            Activity::createActivityLog(
+                'channel_applications',
+                $user,
+                'approved_channel_application',
+                [
+                    'channel_application_id' => $channelApplication->getKey(),
+                    'channel_id' => $channel->getKey(),
+                    'is_new_channel' => $isNewChannel,
+                ]
+            );
         }
 
 
