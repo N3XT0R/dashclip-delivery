bafdec8b0245ee4ea628833ae52b61e27c77dba3
Ilya Beliaev <info@php-dev.info>
2025-12-26 20:12:35 +0100

    bugfix on mailable.

--- app/Events/ActionToken/ActionTokenConsumed.php
+++ app/Events/ActionToken/ActionTokenConsumed.php
@@ -5,10 +5,12 @@ declare(strict_types=1);
 namespace App\Events\ActionToken;
 
 use App\Models\ActionToken;
+use Illuminate\Contracts\Events\ShouldDispatchAfterCommit;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class ActionTokenConsumed
+class ActionTokenConsumed implements ShouldQueue, ShouldDispatchAfterCommit
 {
     use Dispatchable, SerializesModels;
 
