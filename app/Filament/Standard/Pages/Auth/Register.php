e2266d79b2590f901a148ca05889d0231c085ece
Ilya Beliaev <info@php-dev.info>
2025-12-26 12:14:05 +0100

    fix: auto assigning role for registration.

--- app/Filament/Standard/Pages/Auth/Register.php
+++ app/Filament/Standard/Pages/Auth/Register.php
@@ -7,13 +7,10 @@ namespace App\Filament\Standard\Pages\Auth;
 use App\Enum\Guard\GuardEnum;
 use App\Enum\Users\RoleEnum;
 use App\Models\User;
-use App\Repository\RoleRepository;
 use App\Support\FilamentComponents;
 use Filament\Auth\Pages\Register as BaseRegister;
-use Filament\Facades\Filament;
 use Filament\Notifications\Notification;
 use Filament\Schemas\Schema;
-use Illuminate\Support\Facades\Log;
 use Illuminate\Validation\ValidationException;
 
 class Register extends BaseRegister
@@ -45,24 +42,6 @@ class Register extends BaseRegister
 
         /** @var User $user */
         $user = parent::handleRegistration($data);
-        $roleRepository = app(RoleRepository::class);
-
-        try {
-            $user->assignRole($roleRepository->getRoleByRoleEnum(
-                RoleEnum::REGULAR,
-                Filament::getCurrentPanel()?->getAuthGuard()
-            ));
-        } catch (\Throwable $e) {
-            Log::error('Role assignment failed', ['exception' => $e, 'user' => $user]);
-            $user->delete();
-
-
-            $this->getRoleAssignmentFailedNotification(
-                RoleEnum::REGULAR,
-                GuardEnum::DEFAULT
-            )->send();
-        }
-
         return $user;
     }
 
@@ -70,10 +49,12 @@ class Register extends BaseRegister
     {
         return Notification::make()
             ->title(__('auth.register.role_assignment_failed_title'))
-            ->body(__('auth.register.role_assignment_failed_body', [
-                'role' => $role,
-                'guard' => $guard?->value ?: 'web',
-            ]))
+            ->body(
+                __('auth.register.role_assignment_failed_body', [
+                    'role' => $role,
+                    'guard' => $guard?->value ?: 'web',
+                ])
+            )
             ->danger();
     }
 
