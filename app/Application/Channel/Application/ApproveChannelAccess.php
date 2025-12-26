95bc98531fe9051ac0a9427b6dd1ea8676b64b01
Ilya Beliaev <info@php-dev.info>
2025-12-26 15:42:34 +0100

    feat(#131): implementing feature.

--- app/Application/Channel/Application/ApproveChannelAccess.php
+++ app/Application/Channel/Application/ApproveChannelAccess.php
@@ -5,9 +5,15 @@ declare(strict_types=1);
 namespace App\Application\Channel\Application;
 
 use App\Models\ChannelApplication;
+use App\Services\NotificationService;
 
 class ApproveChannelAccess
 {
+    public function __construct(
+        private NotificationService $notificationService
+    ) {
+    }
+
     public function handle(ChannelApplication $channelApplication): void
     {
     }
