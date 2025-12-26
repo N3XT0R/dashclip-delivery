2ce80d16892e8f8ef65c5ec778b3a6a6b1e941db
Ilya Beliaev <info@php-dev.info>
2025-12-26 11:16:56 +0100

    feat(#131): implementing feature.

--- app/Application/Channel/ApproveChannelApplication.php
+++ app/Application/Channel/ApproveChannelApplication.php
@@ -24,7 +24,7 @@ final readonly class ApproveChannelApplication
     public function handle(
         ChannelApplicationModel $application,
         ?User $approvedBy
-    ) {
+    ): void {
         DB::beginTransaction();
 
         try {
