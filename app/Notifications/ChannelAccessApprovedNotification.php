1a58b16400e9869f2e1479352d900649f2013ebe
Ilya Beliaev <info@php-dev.info>
2025-12-26 15:29:46 +0100

    feat(#131): implementing feature.

--- app/Notifications/ChannelAccessApprovedNotification.php
+++ app/Notifications/ChannelAccessApprovedNotification.php
@@ -4,7 +4,34 @@ declare(strict_types=1);
 
 namespace App\Notifications;
 
+use App\Mail\AbstractLoggedMail;
+use App\Mail\ChannelAccessApprovedMail;
+use App\Models\ChannelApplication;
+use App\Models\User;
+use App\Notifications\Contracts\HasToDatabaseContract;
+use App\Notifications\Contracts\HasToMailContract;
+use Illuminate\Database\Eloquent\Model;
+
 class ChannelAccessApprovedNotification extends AbstractUserNotification
+    implements HasToMailContract,
+               HasToDatabaseContract
 {
-    
+    public function __construct(
+        public readonly ChannelApplication $channelApplication
+    ) {
+    }
+
+    public function toDatabase(Model $notifiable): array
+    {
+        return [
+            'application' => $this->channelApplication,
+            'channel' => $this->channelApplication->channel,
+        ];
+    }
+
+    public function toMail(User $notifiable): AbstractLoggedMail
+    {
+        return new ChannelAccessApprovedMail($this->channelApplication)->to($notifiable);
+    }
+
 }
