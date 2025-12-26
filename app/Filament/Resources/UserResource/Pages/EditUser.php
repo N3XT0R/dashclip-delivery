57d3c54bbdb4ac8427723b8f18e1bf1833334d4d
Ilya Beliaev <info@php-dev.info>
2025-12-26 19:12:30 +0100

    feat(#131): implementing feature.

--- app/Filament/Resources/UserResource/Pages/EditUser.php
+++ app/Filament/Resources/UserResource/Pages/EditUser.php
@@ -5,10 +5,20 @@ namespace App\Filament\Resources\UserResource\Pages;
 use App\Filament\Resources\UserResource;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
+use Illuminate\Database\Eloquent\Model;
 
 class EditUser extends EditRecord
 {
     protected static string $resource = UserResource::class;
+
+    protected function handleRecordUpdate(Model $record, array $data): Model
+    {
+        if (empty($data['password'] ?? null)) {
+            unset($data['password']);
+        }
+
+        return parent::handleRecordUpdate($record, $data);
+    }
 
     protected function getHeaderActions(): array
     {
