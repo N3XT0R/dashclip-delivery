1123a0fa3122faf4b9a3711d2476e25ece7a70fc
Ilya Beliaev <info@php-dev.info>
2025-12-27 20:33:06 +0100

    bugfix.

--- app/Application/Channel/Application/ApproveChannelAccess.php
+++ app/Application/Channel/Application/ApproveChannelAccess.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace App\Application\Channel\Application;
 
+use App\Enum\Guard\GuardEnum;
+use App\Enum\Users\RoleEnum;
 use App\Models\ChannelApplication;
+use App\Repository\RoleRepository;
 use App\Services\Channel\ChannelOperatorService;
 use App\Services\NotificationService;
 
@@ -12,12 +15,26 @@ final readonly class ApproveChannelAccess
 {
     public function __construct(
         private NotificationService $notificationService,
-        private ChannelOperatorService $channelOperatorService
+        private ChannelOperatorService $channelOperatorService,
+        private RoleRepository $roleRepository,
     ) {
     }
 
     public function handle(ChannelApplication $channelApplication): void
     {
+        $role = RoleEnum::CHANNEL_OPERATOR;
+        $guard = GuardEnum::STANDARD;
+
+        $user = $channelApplication->user;
+        if (!$this->roleRepository->hasRole($user, $role, $guard)) {
+            $this->roleRepository->giveRoleToUser(
+                $user,
+                $role,
+                $guard
+            );
+        }
+
+
         $this->channelOperatorService->approveUserChannelAccess(
             $channelApplication->user,
             $channelApplication->channel
