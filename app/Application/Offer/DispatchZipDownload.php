918b2533a89ba2296956f4ca4b01e0c83ac17cfa
Ilya Beliaev <info@php-dev.info>
2025-12-27 15:48:21 +0100

    tests(#131): adding tests for feature.

--- app/Application/Offer/DispatchZipDownload.php
+++ app/Application/Offer/DispatchZipDownload.php
@@ -8,6 +8,12 @@ use Filament\Pages\Page;
 
 class DispatchZipDownload
 {
+    /**
+     * Handle the dispatching of a zip download for the given assignment IDs.
+     * @param Page $page
+     * @param iterable $assignmentIds
+     * @return void
+     */
     public function handle(Page $page, iterable $assignmentIds): void
     {
         $page->dispatch('zip-download', [
