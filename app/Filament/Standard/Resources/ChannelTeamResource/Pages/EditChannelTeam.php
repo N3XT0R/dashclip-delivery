f482cdb7b3bea13eb1a74b85ab38a9da377819fc
Ilya Beliaev <info@php-dev.info>
2025-12-28 15:17:07 +0100

    redirect back

--- app/Filament/Standard/Resources/ChannelTeamResource/Pages/EditChannelTeam.php
+++ app/Filament/Standard/Resources/ChannelTeamResource/Pages/EditChannelTeam.php
@@ -16,4 +16,9 @@ class EditChannelTeam extends EditRecord
             Actions\DeleteAction::make(),
         ];
     }
+
+    protected function getRedirectUrl(): string
+    {
+        return self::getResource()::getUrl('index');
+    }
 }
