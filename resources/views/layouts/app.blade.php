bb439b52cc854d82f62240aa9cfe672f6bb921fa
Ilya Beliaev <info@php-dev.info>
2025-12-28 12:16:35 +0100

    seo.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,15 +1,20 @@
 @php use App\Facades\Version; @endphp
-        <!doctype html>
+    <!doctype html>
 <html lang="de">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>@yield('title', config('app.name', 'App'))</title>
+    <title>@yield('title', config('app.name', 'DashClip Delivery'))</title>
+    {{-- SEO --}}
+    @yield('seo')
+
+    {{-- Icons --}}
     <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
     <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('images/icons/apple-touch-icon.png') }}">
     <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('images/icons/favicon-32x32.png') }}">
     <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('images/icons/favicon-16x16.png') }}">
+
     @yield('head')
     @vite(['resources/css/app.css','resources/js/app.js'])
     @stack('styles')
