a70de4c4c62494b366373aaf91d0df6c4d6bac99
Ilya Beliaev <info@php-dev.info>
2025-12-29 14:42:27 +0100

    feat: channel settings

---
+++ database/migrations/2025_12_29_144032_create_channel_configs_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('channel_configs', static function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('channel_id')->constrained()->cascadeOnDelete();
+            $table->string('key');
+            $table->text('value')->nullable();
+            $table->string('type')->default('bool');
+            $table->timestamps();
+
+            $table->unique(['channel_id', 'key']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('channel_configs');
+    }
+};
