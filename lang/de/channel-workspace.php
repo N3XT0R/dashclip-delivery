f3b58fe435769ed475f5afca1546b21270c735ec
Ilya Beliaev <info@php-dev.info>
2025-12-29 15:08:44 +0100

    feat: channel settings

--- lang/de/channel-workspace.php
+++ lang/de/channel-workspace.php
@@ -3,4 +3,7 @@
 declare(strict_types=1);
 return [
     'title' => 'Kanaleinstellungen',
+    'channel_general_settings' => [
+        'title' => 'Allgemeine Einstellungen',
+    ],
 ];
--- lang/en/channel-workspace.php
+++ lang/en/channel-workspace.php
@@ -3,4 +3,7 @@
 declare(strict_types=1);
 return [
     'title' => 'Channel Settings',
+    'channel_general_settings' => [
+        'title' => 'General Settings',
+    ],
 ];
