6ef5a198ff5db781cef59a15b9a2c0a7bd8f24f6
Ilya Beliaev <info@php-dev.info>
2025-12-30 12:35:04 +0100

    feat: api added

---
+++ app/Services/API/ClientService.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services\API;
+
+use App\Models\User;
+use Laravel\Passport\Client;
+use Laravel\Passport\ClientRepository;
+
+class ClientService
+{
+    public function __construct(private ClientRepository $clientRepository)
+    {
+    }
+
+    public function createPersonalAccessClient(string $name, User $user): Client
+    {
+        $client = $this->clientRepository->createPersonalAccessGrantClient(
+            $name,
+        );
+
+        $client->owner = $user;
+        $client->save();
+        return $client;
+    }
+}
