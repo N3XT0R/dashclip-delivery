dae09d4ef3caab1a9bde843473f86a825011e6d1
Ilya Beliaev <info@php-dev.info>
2026-02-10 00:50:11 +0100

    feat: new activity log added as composer package.

--- app/Filament/Resources/UserResource/Pages/ListUserActivities.php
+++ app/Filament/Resources/UserResource/Pages/ListUserActivities.php
@@ -4,13 +4,10 @@ declare(strict_types=1);
 
 namespace App\Filament\Resources\UserResource\Pages;
 
-use App\Filament\Clusters\ActivityWorkspace;
 use App\Filament\Resources\ActivityResource\Pages\ListActivities;
 use App\Filament\Resources\UserResource;
 
 class ListUserActivities extends ListActivities
 {
     protected static string $resource = UserResource::class;
-
-    protected static ?string $cluster = ActivityWorkspace::class;
 }
