fee83d78b8d4b3238bcc4a9933bdebce06f070b5
Ilya Beliaev <info@php-dev.info>
2026-03-01 19:44:48 +0100

    refactor: ingest flow. #265

--- app/Console/Commands/VideoProcessing/CalculateHashCommand.php
+++ app/Console/Commands/VideoProcessing/CalculateHashCommand.php
@@ -4,11 +4,22 @@ declare(strict_types=1);
 
 namespace App\Console\Commands\VideoProcessing;
 
+use App\Application\Video\UpdateVideoHash;
+use App\Repository\VideoRepository;
 use Illuminate\Console\Command;
 
 class CalculateHashCommand extends Command
 {
-    public function handle(): void
-    {
+
+    protected $signature = 'videos:calculate-hash';
+
+    public function handle(
+        VideoRepository $videoRepository,
+        UpdateVideoHash $updateVideoHash
+    ): void {
+        $videos = $videoRepository->getVideosWhereHashIsEmpty();
+        foreach ($videos as $video) {
+            $updateVideoHash->handle($video);
+        }
     }
 }
