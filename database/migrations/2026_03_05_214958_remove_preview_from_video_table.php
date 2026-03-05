249392abb609002b38b0c7611316acde4f0f0edc
Ilya Beliaev <info@php-dev.info>
2026-03-05 21:53:02 +0100

    refactor: ingest flow. #265

--- database/migrations/2026_03_05_214958_remove_preview_from_video_table.php
+++ database/migrations/2026_03_05_214958_remove_preview_from_video_table.php
@@ -4,15 +4,14 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::table('video', static function (Blueprint $table) {
-            //
+            $table->dropColumn('path');
         });
     }
 
@@ -22,7 +21,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('video', static function (Blueprint $table) {
-            //
+            $table->string('path')->nullable()->after('bytes');
         });
     }
 };
