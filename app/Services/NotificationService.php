11297870aa69cc3ab16070dcbc1b9732cae3fcfb
Ilya Beliaev <info@php-dev.info>
2026-03-08 00:39:13 +0100

    refactor: ingest flow. #265

--- app/Services/NotificationService.php
+++ app/Services/NotificationService.php
@@ -9,12 +9,13 @@ use App\Models\User;
 use App\Models\Video;
 use App\Notifications\ChannelAccessApprovedNotification;
 use App\Notifications\UserUploadDuplicatedNotification;
+use App\Notifications\UserUploadProceedNotification;
 
 class NotificationService
 {
     /**
      * Send channel access approved notification to the user.
-     * @param  ChannelApplication  $channelApplication
+     * @param ChannelApplication $channelApplication
      * @return void
      */
     public function notifyChannelAccessApproved(ChannelApplication $channelApplication): void
@@ -25,15 +26,33 @@ class NotificationService
 
     /**
      * Send duplicated upload notification to the user.
-     * @param  Video  $video
-     * @param  User  $user
+     * @param Video $video
+     * @param User $user
      * @return void
      */
     public function notifyDuplicatedUpload(User $user, Video $video): void
     {
-        $user->notify(new UserUploadDuplicatedNotification(
-            filename: $video->original_name,
-            note: __('user_upload_duplicated.body', ['filename' => $video->original_name])
-        ));
+        $user->notify(
+            new UserUploadDuplicatedNotification(
+                filename: $video->original_name,
+                note: __('user_upload_duplicated.body', ['filename' => $video->original_name])
+            )
+        );
+    }
+
+    /**
+     * Send upload proceed notification to the user.
+     * @param User $user
+     * @param Video $video
+     * @return void
+     */
+    public function notifyUserUploadComplete(User $user, Video $video): void
+    {
+        $user->notify(
+            new UserUploadProceedNotification(
+                filename: $video->original_name,
+                note: 'Alles erfolgreich abgeschlossen.'
+            )
+        );
     }
 }
