0e2309126c716e2584d8a3122a0f1f301585f603
Ilya Beliaev <info@php-dev.info>
2026-03-08 01:24:09 +0100

    refactor: ingest flow. #265

--- app/Services/PathBuilderService.php
+++ app/Services/PathBuilderService.php
@@ -10,61 +10,61 @@ class PathBuilderService
 {
     /**
      * Get the video path by its hash and extension.
-     * @param string $hash
-     * @param string $ext
+     * @param  string  $hash
+     * @param  string  $ext
      * @return string
      */
     public function forVideo(string $hash, string $ext): string
     {
-        $sub = substr($hash, 0, 2) . '/' . substr($hash, 2, 2);
+        $sub = substr($hash, 0, 2).'/'.substr($hash, 2, 2);
         return sprintf('videos/%s/%s%s', $sub, $hash, $ext !== '' ? ".{$ext}" : '');
     }
 
     /**
      * Get the preview path by video ID and time range.
-     * @param int $id
-     * @param int $start
-     * @param int $end
+     * @param  int  $id
+     * @param  int  $start
+     * @param  int  $end
      * @return string
      * @deprecated use forPreviewByClip instead
      */
     public function forPreview(int $id, int $start, int $end): string
     {
-        $hash = hash('sha256', $id . '_' . $start . '_' . $end);
+        $hash = hash('sha256', $id.'_'.$start.'_'.$end);
         return sprintf('previews/%s.mp4', $hash);
     }
 
     /**
      * Get the preview path by video file hash.
-     * @param string $fileHash
+     * @param  string  $fileHash
      * @return string
      * @note This is used to get the preview path based on the video file hash, it will replace forPreview in the future.
      * @deprecated use forPreviewByClip instead
      */
     public function forPreviewByHash(string $fileHash): string
     {
-        $sub = substr($fileHash, 0, 2) . '/' . substr($fileHash, 2, 2);
+        $sub = substr($fileHash, 0, 2).'/'.substr($fileHash, 2, 2);
         return sprintf('previews/%s/%s.mp4', $sub, $fileHash);
     }
 
     /**
      * Get the preview path by clip data.
-     * @param Clip $clip
+     * @param  Clip  $clip
      * @return string
      */
     public function forPreviewByClip(Clip $clip): string
     {
-        $hash = hash('sha256', $clip->getKey());
-        $sub = substr($hash, 0, 2) . '/' . substr($hash, 2, 2);
+        $hash = hash('sha256', (string)$clip->getKey());
+        $sub = substr($hash, 0, 2).'/'.substr($hash, 2, 2);
         $ext = strtolower($clip->video->ext);
 
-        return sprintf('%s/%s.' . $ext, $sub, $hash);
+        return sprintf('%s/%s.'.$ext, $sub, $hash);
     }
 
     /**
      * Build a path for Dropbox storage.
-     * @param string $basePath
-     * @param string $relativePath
+     * @param  string  $basePath
+     * @param  string  $relativePath
      * @return string
      */
     public function forDropbox(string $basePath, string $relativePath): string
@@ -74,11 +74,11 @@ class PathBuilderService
 
     /**
      * Join multiple path parts into a single path.
-     * @param string ...$parts
+     * @param  string  ...$parts
      * @return string
      */
     public function join(string ...$parts): string
     {
-        return '/' . trim(implode('/', array_filter($parts)), '/');
+        return '/'.trim(implode('/', array_filter($parts)), '/');
     }
 }
