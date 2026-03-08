324560c622e16529e98bef398900c0f4087d1ce6
Ilya Beliaev <info@php-dev.info>
2026-03-08 01:12:05 +0100

    refactor: ingest flow. #265

--- app/Events/Video/VideoStored.php
+++ app/Events/Video/VideoStored.php
@@ -4,23 +4,9 @@ declare(strict_types=1);
 
 namespace App\Events\Video;
 
-use App\Models\User;
-use App\Models\Video;
-use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Foundation\Events\Dispatchable;
-use Illuminate\Queue\SerializesModels;
-
 /**
  * Event that is fired when a video has been stored in the storage and database, and is ready for processing.
  */
-readonly class VideoStored
+readonly class VideoStored extends VideoCreated
 {
-    use Dispatchable, InteractsWithSockets, SerializesModels;
-
-
-    public function __construct(
-        public Video $video,
-        public ?User $user = null,
-    ) {
-    }
 }
