e2df1a702f3d7685ec9e48b36bc91237791983e7
Ilya Beliaev <info@php-dev.info>
2026-03-08 13:00:50 +0100

    refactor: ingest flow. #265

--- app/Events/Video/VideoQueuedForIngest.php
+++ app/Events/Video/VideoQueuedForIngest.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 namespace App\Events\Video;
 
 /**
- * This event is fired when a video is created for ingest. It is used to trigger the ingest process.
+ * This event is fired when a video is queued for ingest.
+ * It extends the VideoCreated event, which means it will have all the same properties and methods as VideoCreated,
+ * but it can be used to specifically indicate that the video is now in the ingest queue.
  */
 readonly class VideoQueuedForIngest extends VideoCreated
 {
