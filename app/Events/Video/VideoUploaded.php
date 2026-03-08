22fc684cbaab1ee968d3f5285e5ebd650bc03800
Ilya Beliaev <info@php-dev.info>
2026-03-08 01:10:25 +0100

    refactor: ingest flow. #265

--- app/Events/Video/VideoUploaded.php
+++ app/Events/Video/VideoUploaded.php
@@ -4,7 +4,20 @@ declare(strict_types=1);
 
 namespace App\Events\Video;
 
-readonly class VideoUploaded extends VideoCreated
-{
+use App\Models\User;
+use App\Models\Video;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
 
+readonly class VideoUploaded
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+
+    public function __construct(
+        public Video $video,
+        public ?User $user = null,
+    ) {
+    }
 }
