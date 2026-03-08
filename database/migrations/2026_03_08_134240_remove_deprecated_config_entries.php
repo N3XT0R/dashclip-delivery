ef0542ca981420adbc5fa25db6ef4c12c523a84d
Ilya Beliaev <info@php-dev.info>
2026-03-08 13:45:21 +0100

    refactor: ingest flow. #265

--- database/migrations/2026_03_08_134240_remove_deprecated_config_entries.php
+++ database/migrations/2026_03_08_134240_remove_deprecated_config_entries.php
@@ -1,17 +1,19 @@
 <?php
 
+use Carbon\Carbon;
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        //
+        DB::table('configs')
+            ->whereIn('key', [
+                'email_reminder',
+            ])
+            ->delete();
     }
 
     /**
@@ -19,6 +21,16 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        $timestamp = Carbon::now()->format('Y-m-d H:i:s');
+        $categoryId = DB::table('config_categories')->where('slug', 'email')->value('id');
+        DB::table('configs')->insert([
+            'key' => 'email_reminder',
+            'value' => 0,
+            'cast_type' => 'bool',
+            'config_category_id' => $categoryId,
+            'created_at' => $timestamp,
+            'updated_at' => $timestamp,
+            'is_visible' => 1,
+        ]);
     }
 };
