16c9c3358172f0ec2b241546e8c8ad4e588f2ad6
Ilya Beliaev <info@php-dev.info>
2026-03-10 21:17:34 +0100

    refactor: ingest flow. #265

--- routes/console.php
+++ routes/console.php
@@ -2,14 +2,7 @@
 
 
 use App\Console\Commands;
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
-
-
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
 
 Schedule::command(Commands\WeeklyRun::class)
     ->mondays()
@@ -23,12 +16,10 @@ Schedule::command(Commands\AssignUploader::class)->everyTenMinutes();
 Schedule::command(Commands\RefreshDropboxToken::class)
     ->everyMinute();
 
-Schedule::command('mail:scan-replies')->everyTenMinutes();
-Schedule::command('clean:database')->dailyAt('02:00');
-Schedule::command('assign:videos-to-teams')->everyFifteenMinutes();
+Schedule::command(Commands\ScanMailReplies::class)->everyTenMinutes();
+Schedule::command(Commands\CleanUpDatabaseCommand::class)->dailyAt('02:00');
+Schedule::command(Commands\AssignVideosToTeams::class)->everyFifteenMinutes();
 
 //video processing
-Schedule::command('video:process-videos')->everyMinute();
-
 Schedule::command(Commands\VideoProcessing\RequeueStaleRunningCommand::class)->everyFifteenMinutes();
 Schedule::command(Commands\VideoProcessing\RequeueFailedVideosCommand::class)->everyFifteenMinutes();
