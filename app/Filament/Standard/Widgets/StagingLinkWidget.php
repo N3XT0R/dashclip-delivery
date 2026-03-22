09ebf59d1f77f1b7e54ce77fa781445de73b1019
Ilya Beliaev <info@php-dev.info>
2026-03-22 22:44:07 +0100

    feat: added widget for staging environment.

---
+++ app/Filament/Standard/Widgets/StagingLinkWidget.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Filament\Standard\Widgets;
+
+use Filament\Widgets\Widget;
+
+class StagingLinkWidget extends Widget
+{
+    protected string $view = 'filament.standard.widgets.staging-link';
+}
