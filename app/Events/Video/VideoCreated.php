28589df575e1aeccb548ccab29bfbe99d99177b6
Ilya Beliaev <info@php-dev.info>
2026-03-29 03:47:15 +0200

    replaced config strings with config constants.

--- app/Events/Video/VideoCreated.php
+++ app/Events/Video/VideoCreated.php
@@ -10,7 +10,7 @@ use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-readonly class VideoCreated
+class VideoCreated
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
--- app/Events/Video/VideoQueuedForIngest.php
+++ app/Events/Video/VideoQueuedForIngest.php
@@ -9,6 +9,6 @@ namespace App\Events\Video;
  * It extends the VideoCreated event, which means it will have all the same properties and methods as VideoCreated,
  * but it can be used to specifically indicate that the video is now in the ingest queue.
  */
-readonly class VideoQueuedForIngest extends VideoCreated
+class VideoQueuedForIngest extends VideoCreated
 {
 }
