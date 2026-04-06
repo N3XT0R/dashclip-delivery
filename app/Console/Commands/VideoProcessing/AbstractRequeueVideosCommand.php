88096dca808f0fa41c2a52f223fabe2a141e9049
Ilya Beliaev <info@php-dev.info>
2026-04-06 10:02:19 +0200

    added more exception handling.

--- app/Console/Commands/VideoProcessing/AbstractRequeueVideosCommand.php
+++ app/Console/Commands/VideoProcessing/AbstractRequeueVideosCommand.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Console\Commands\VideoProcessing;
 
 use App\Events\Video\VideoQueuedForIngest;
+use App\Models\Video;
 use App\Repository\VideoRepository;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Log;
@@ -15,8 +16,24 @@ abstract class AbstractRequeueVideosCommand extends Command
     final public function handle(VideoRepository $videoRepository): int
     {
         try {
+            /**
+             * @var Video $video
+             */
             foreach ($this->getVideos($videoRepository) as $video) {
-                VideoQueuedForIngest::dispatch($video);
+                $disk = $video->getDisk();
+                if (!$disk->exists($video->getAttribute('path'))) {
+                    $video->delete();
+                    Log::warning(
+                        'Video file missing during requeue, video deleted',
+                        [
+                            'video_id' => $video->getKey(),
+                            'disk' => $video->getAttribute('disk'),
+                            'path' => $video->getAttribute('path')
+                        ]
+                    );
+                } else {
+                    VideoQueuedForIngest::dispatch($video);
+                }
             }
         } catch (\Throwable $e) {
             Log::error($this->getErrorLogMessage(), [
