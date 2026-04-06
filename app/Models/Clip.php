7da18f92eb3b02b0e6911fd41828e3a7871dc2e7
Ilya Beliaev <info@php-dev.info>
2026-04-06 09:36:01 +0200

    added soft deletes to video/clip.

--- app/Models/Clip.php
+++ app/Models/Clip.php
@@ -9,11 +9,13 @@ use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Facades\Storage;
 
 class Clip extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 
     protected $fillable = [
         'video_id',
