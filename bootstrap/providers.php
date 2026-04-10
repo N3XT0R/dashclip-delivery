28a0268083e15caa59c85a6b0de28c1d5f15b01c
Ilya Beliaev <info@php-dev.info>
2026-04-10 23:01:22 +0200

    added laravel/horizon.

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,11 +2,12 @@
 
 return [
     App\Providers\AppServiceProvider::class,
-    App\Providers\ServiceServiceProvider::class,
-    App\Providers\PolicyServiceProvider::class,
-    App\Providers\ObserverServiceProvider::class,
-    App\Providers\IngestServiceProvider::class,
     App\Providers\Filament\AdminPanelProvider::class,
     App\Providers\Filament\PanelUserPanelProvider::class,
+    App\Providers\HorizonServiceProvider::class,
+    App\Providers\IngestServiceProvider::class,
+    App\Providers\ObserverServiceProvider::class,
+    App\Providers\PolicyServiceProvider::class,
     App\Providers\QueueEventServiceProvider::class,
+    App\Providers\ServiceServiceProvider::class,
 ];
