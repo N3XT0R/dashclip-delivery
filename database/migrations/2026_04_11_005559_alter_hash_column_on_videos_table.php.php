d32cde5dd4eb9cfdf7060459e7bed7f5b4e34f3e
Ilya Beliaev <info@php-dev.info>
2026-04-11 01:05:00 +0200

    added laravel/horizon.

--- database/migrations/2026_04_11_005559_alter_hash_column_on_videos_table.php.php
+++ database/migrations/2026_04_11_005559_alter_hash_column_on_videos_table.php.php
@@ -11,8 +11,8 @@ return new class extends Migration {
     public function up(): void
     {
         Schema::table('videos', static function (Blueprint $table) {
-            $table->dropUnique(['hash']);
-            $table->string('hash', 64)->index()->change();
+            //$table->dropUnique(['hash']);
+            $table->string('hash', 255)->index()->change();
         });
     }
 
@@ -22,6 +22,7 @@ return new class extends Migration {
     public function down(): void
     {
         Schema::table('videos', static function (Blueprint $table) {
+            $table->dropIndex(['hash']);
             $table->string('hash', 64)->unique()->change();
         });
     }
